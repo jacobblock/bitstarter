dec2fd53458ad674990a4b6d951152258c75ec2a
Ubuntu <ubuntu@ip-10-164-26-210.ec2.internal>
2013-07-11 12:38:04 +0000

    Fixed importing fs and encoding.

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
-import fs = module('fs');
+var fs = require('fs');
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString('utf-8'));
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
